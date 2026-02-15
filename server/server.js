022844482dc2b918ef1c9c63fba266bd1f3a7d2e
CLARA AROLIN VICTORIA F <claraxavierkec2428@gmail.com>
2026-02-15 18:40:45 +0530

    Initial commit - Secure Authentication System

---
+++ server/server.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+require('./db');
+
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/auth', require('./routes/auth'));  // ← THIS LINE MUST EXIST
+
+app.get('/', (req, res) => {
+  res.send("Server is running");
+});
+app.listen(process.env.PORT || 5000, () => {
+  console.log(`Server started on port ${process.env.PORT || 5000}`);
+});
+
+
